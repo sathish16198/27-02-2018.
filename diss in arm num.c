68454ce2700ab2578b6e80e8daadd9c9f6063575
sathish16198 <35728168+sathish16198@users.noreply.github.com>
2018-02-27 16:31:28 +0530

    Update diss in arm num.c

--- diss in arm num.c
+++ diss in arm num.c
@@ -1,10 +1,14 @@
 #include<stdio.h>
+
 #include<math.h>
+
 int main() 
 {
     int n1,n2,sum=0,count=0,temp=0,temp1=0,i,rem=0;
-    scanf("%d %d",&n1,&n2);
-    for(i=n1;i<n2;i++)
+   
+	scanf("%d %d",&n1,&n2);
+    
+	for(i=n1;i<n2;i++)
     {
         temp=i;
         temp1=i;
@@ -13,17 +17,21 @@ int main()
             count++;
             temp=temp/10;
         }
-        while(temp1!=0)
+       
+		while(temp1!=0)
         {
-            rem=temp1%10;
+           
+		rem=temp1%10;
             sum=sum+pow(rem,count);
             temp1=temp1/10;
         }
         if(sum==i)
         {
             printf("%d",sum);
-        }
-       sum=0;
+     
+	}
+       
+		sum=0;
        count=0;
     }
 	return 0;
